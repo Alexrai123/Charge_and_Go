a83f75ddcaff95b7898b2a05cffffa23e550b5e2
Alexrai123 <alexhudita@gmail.com>
2024-05-03 00:37:00 +0300

    first commit

--- app/src/main/java/com/example/licenta_copie/Database/Repository/ReservationRepository.kt
+++ app/src/main/java/com/example/licenta_copie/Database/Repository/ReservationRepository.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface ReservationRepository {
     fun getAllReservationsStream(): Flow<List<Reservation>>
     fun getReservationById(id: Int): Flow<Reservation?>
+    fun getAllReservationsByData(): Flow<List<Reservation>>
     suspend fun insertReservation(reservation: Reservation)
     suspend fun deleteReservation(reservation: Reservation)
     suspend fun updateReservation(reservation: Reservation)
